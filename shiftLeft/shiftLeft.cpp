14086d0862fb18da79c89a291386eea359624b38
Gleb Volkov <glebchikvlkv@gmail.com>
2020-11-04 19:38:07 +0200

    first commit

---
+++ shiftLeft/shiftLeft.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int A[100];
+	int N;
+	cout << "Введите длину массива: ";
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cout << "A[" << i << "]=";
+		cin >> A[i];
+	}
+	int k, j;
+	cout << "Введите длину на которую необходимо сдвинуть элементы массива: ";
+	cin >> k;
+	k = k % N;
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < N-1; j++) {
+			int t = A[j];
+			A[j] = A[j + 1];
+			A[j + 1] = t;
+		}
+	}
+	for (int i = 0; i < N; i++) cout << A[i] << ' ';
+}
+
